59de1e8ad50a7a0077290161cb555adae6675d88
bossvirus03 <nguyenhuuloi17032004@gmail.com>
2024-02-07 22:15:48 +0700

    first commit

---
+++ assets/scrcipts/main.js
@@ -0,0 +1,39 @@
+var length = 1;
+document.addEventListener("DOMContentLoaded", function () {
+  // Lấy phần tử cần hover
+  var btnNo = document.getElementById("btn-no");
+  var btnYes = document.getElementById("btn-yes");
+  var imgQR = document.getElementById("qr");
+
+  // bắt sự kiện click
+  btnYes.addEventListener("click", function () {
+    btnNo.style.display = "none";
+    btnYes.style.display = "none";
+    imgQR.style.display = "flex";
+  });
+
+  // Bắt sự kiện hover bắt đầu
+  btnNo.addEventListener("mouseover", function () {
+    moveButtonRandomly(btnNo);
+    changeSize(btnYes);
+  });
+
+  function moveButtonRandomly(button) {
+    const maxX = window.innerWidth - button.clientWidth;
+    const maxY = window.innerHeight - button.clientHeight;
+
+    const newX = Math.floor(Math.random() * 500);
+    const newY = Math.floor(Math.random() * 500);
+
+    button.style.left = `${newX}px`;
+    button.style.top = `${newY}px`;
+  }
+
+  // Đặt kích thước cho nút
+  function changeSize(button) {
+    button.style.width =
+      button.offsetWidth + (button.offsetWidth * 40) / 100 + "px";
+    button.style.height =
+      button.offsetHeight + (button.offsetHeight * 40) / 100 + "px";
+  }
+});
